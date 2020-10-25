aa1046c19d73d6bdad298128c176301bbb180e6e
Akash_Pol <65165883+02akashpol@users.noreply.github.com>
2020-10-26 00:41:37 +0530

    Assignment files are uploaded.

---
+++ Day_10/src/main/java/com/Main2.java
@@ -0,0 +1,35 @@
+package com;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.dto.Employee;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		Configuration cfg = new Configuration().configure();
+		StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder();
+		ssrb.applySettings(cfg.getProperties());
+		StandardServiceRegistry ssr = ssrb.build();
+		SessionFactory sf = cfg.buildSessionFactory(ssr);
+		Session s = sf.openSession();
+		Transaction tr = s.beginTransaction();
+		
+		Employee emp = new Employee(104, "Akash Pol", 60000.25f, "IT", new Date());
+		
+		s.delete(emp);
+		tr.commit();
+		s.close();
+		sf.close();
+	
+	} 
+
+}
